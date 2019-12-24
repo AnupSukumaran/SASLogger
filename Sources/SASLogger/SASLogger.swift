fe9bcfc41b891e90aabf186906e01b977bed1bba
Anup Sukumaran <anup.sukumaran9@gmail.com>
2019-12-24 13:05:03 +0530

    - Dec 24, 2019 (1) - First Commit

--- Sources/SASLogger/SASLogger.swift
+++ Sources/SASLogger/SASLogger.swift
@@ -1,3 +1,43 @@
-struct SASLogger {
-    var text = "Hello, World!"
+public struct Logger {
+    
+    public static let showPrintValue = true
+       
+      public enum types: String {
+           case info =  "LOG"
+           case warning =  "WARNING"
+           case error = "ERROR"
+       }
+       
+      public enum Component {
+           case file
+           case function
+           case line
+           
+           func enabled() -> Bool {
+               return Logger.components.contains(self)
+           }
+       }
+
+      public static var components: [Component] = [.file,.function,.line]
+       
+      public static func pnt(_ msg: String,_ function: String = #function,_ line: Int = #line ) {
+           
+           #if DEBUG
+           if showPrintValue {
+               print("Func -> \(function),line -> \(line)👇 \n Result -> \(msg)")
+           }
+           #endif
+           
+       }
+       
+      public static func p(_ msg: String) {
+           
+           #if DEBUG
+           if showPrintValue {
+               print("\(msg)")
+           }
+           #endif
+           
+       }
+    
 }
